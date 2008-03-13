e41d30090f6b7fed07f175e3755c77b74e3a3faa
Scott Raymond <sco@scottraymond.net>
2008-03-13 18:59:57 -0500

    cleaned up comments and TODO

--- lib/fakebook.rb
+++ lib/fakebook.rb
@@ -17,20 +17,15 @@ module Fakebook
       @default_params = params.merge(:in_canvas => 1, :expires => 0, :added => 1)
     end
 
-    # todo: use rack's logger mechanism?
     def request(path, params={})
       url = URI.parse(@callback_url + path.gsub(Regexp.new('^' + @canvas_path), ''))
       path_and_query_string = "#{url.path}?#{url.query}"
       body = sign(params).map{ |a| "#{a.first}=#{a.last}" }.join('&')
-      #start_time = Time.now
       response = Net::HTTP.new(url.host, url.port).post(path_and_query_string, body)
-      #end_time = Time.now
-      #STDERR.puts "[#{end_time - start_time}] #{path}"
       response
     end
 
     private
-          
       def sign(params)
         params_to_sign = (@default_params || {}).merge(:time => Time.now.to_f)
         raw_string = params_to_sign.map{ |pair| pair.join('=') }.sort.join + @app_secret
@@ -44,7 +39,14 @@ module Fakebook
 
   class Proxy
     
-    # this doesn't seem right
+    def initialize(canvas_url, callback_url, app_secret, default_params={})
+      @app_id = '2431403991'
+      @canvas_url = URI.parse(canvas_url)
+      @application = Application.new(callback_url, @canvas_url.path, app_secret)
+      @application.set_default_params(default_params)
+      STDERR.puts "Starting Fakebook proxy from #{canvas_url} to #{callback_url}"
+    end
+
     def self.rack(canvas_url, callback_url, app_secret, default_params={})
       app = Rack::Builder.new do 
         use Rack::ShowExceptions 
@@ -52,14 +54,6 @@ module Fakebook
         run Proxy.new(canvas_url, callback_url, app_secret, default_params)
       end
       Rack::Handler::Mongrel.run(app, :Port => URI.parse(canvas_url).port)
-    end
-    
-    def initialize(canvas_url, callback_url, app_secret, default_params={})
-      @app_id = '2431403991'
-      @canvas_url = URI.parse(canvas_url)
-      @application = Application.new(callback_url, @canvas_url.path, app_secret)
-      @application.set_default_params(default_params)
-      STDERR.puts "Starting Fakebook proxy from #{canvas_url} to #{callback_url}"
     end
 
     def call(env)
@@ -77,7 +71,6 @@ module Fakebook
     end
 
     private
-
       def parse_fbml(body, template='standard.html.erb')
         body.gsub! /<fb:name .*?>/, '[name]'
         body.gsub! /<fb:title>(.*)<\/fb:title>/, ''
