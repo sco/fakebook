30035e733849a101fd4e6094d3c9c93a268166af
Scott Raymond <sco@scottraymond.net>
2008-03-21 15:15:35 -0500

    standardizing parameter names

--- lib/fakebook.rb
+++ lib/fakebook.rb
@@ -6,28 +6,28 @@ require 'rubygems'
 require 'rack'
 
 # Valid options: 
-#   :callback_url
-#   :canvas_path
-#   :app_secret
+#   :callback
+#   :canvas
+#   :secret
 #   :fb_params
-#   :template_file
+#   :template
 #
 class Fakebook
   
   Version = '0.1.0'
   
-  attr_accessor :callback_url, :canvas_path, :app_secret, :fb_params, :template_file
+  attr_accessor :callback, :canvas, :secret, :fb_params, :template
 
   #def self.new(*args)
   #  Rack::Static.new(super, :urls => ["/fakebook-assets"], :root => File.join(File.expand_path(File.dirname(__FILE__)), "..", "lib"))
   #end
 
   def initialize(options = {})
-    @callback_url  =  options[:callback_url]
-    @canvas_path   =  options[:canvas_path]
-    @app_secret    =  options[:app_secret]
-    @fb_params     = (options[:fb_params] || {}).merge(:in_canvas => 1, :expires => 0, :added => 1)
-    @template_file =  options[:template_file] || File.join(File.dirname(__FILE__), "templates", "standard.html.erb")
+    @callback  =  options[:callback]
+    @canvas    =  options[:canvas]
+    @secret    =  options[:secret]
+    @fb_params = (options[:fb_params] || {}).merge(:in_canvas => 1, :expires => 0, :added => 1)
+    @template  =  options[:template] || File.join(File.dirname(__FILE__), "templates", "standard.html.erb")
   end
 
   # Implements the Rack interface. Takes a hash representing the environment; returns an 
@@ -50,7 +50,7 @@ class Fakebook
   
   # Takes a path and optional params; returns the response body.
   def request(path, params={})
-    url = URI.parse(@callback_url + path.gsub(Regexp.new('^' + @canvas_path), ''))
+    url = URI.parse(@callback + path.gsub(Regexp.new('^' + @canvas), ''))
     path_and_query_string = "#{url.path}?#{url.query}"
     request_body = sign(params).map{ |a| "#{a.first}=#{a.last}" }.join('&')
     response = Net::HTTP.new(url.host, url.port).post(path_and_query_string, request_body)
@@ -61,7 +61,7 @@ class Fakebook
   
     def sign(params)
       params_to_sign = (@fb_params || {}).merge(:time => Time.now.to_f)
-      raw_string = params_to_sign.map{ |pair| pair.join('=') }.sort.join + @app_secret
+      raw_string = params_to_sign.map{ |pair| pair.join('=') }.sort.join + @secret
       signature = Digest::MD5.hexdigest(raw_string)
       params_to_sign.each { |k,v| params["fb_sig_" + k.to_s] = v.to_s }
       params['fb_sig'] = signature
@@ -72,11 +72,11 @@ class Fakebook
       body.gsub! /<fb:name .*?>/, '[name]'
       body.gsub! /<fb:title>(.*)<\/fb:title>/, ''
       title = $1
-      template.result(binding)
+      erb.result(binding)
     end
     
-    def template
-      @template ||= ERB.new(File.read(@template_file))
+    def erb
+      @erb ||= ERB.new(File.read(@template))
     end
 
 end
